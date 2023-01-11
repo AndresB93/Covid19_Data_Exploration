77ab11eabce5fa7cccbe00d363105cd6dd0dabb0
AndresB93 <andresb93@outlook.com>
2023-01-10 21:33:35 -0500

    New SQL

--- covid19dataexploration.sql
+++ covid19dataexploration.sql
@@ -49,3 +49,74 @@ WHERE continent is not null
 GROUP BY Location
 ORDER BY TotalDeathCount desc
 
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+SELECT *
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE continent is not null 
+ORDER BY 3,4
+
+ -- Select first data that we are going to use 
+
+SELECT location, date, total_cases, new_cases, total_deaths, population,
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE continent is not null
+ORDER BY 1,2
+
+-- Looking at Total Cases vs Total Deaths
+-- Shows death rate of each location.(My case Colombia)
+
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Rate
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE location='Colombia' and continent is not null
+ORDER BY 1,2
+
+
+-- Looking at Total Cases vs Population
+-- Shows infection rate of the total population in Colombia
+
+SELECT location, date, Population, total_cases, (total_cases/Population)*100 as Infection_Rate
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE location='Colombia' and continent is not null
+ORDER BY Infection_Rate desc
+
+-- Looking at Countries with highest infection rate compared to population
+
+
+SELECT location, Population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/Population))*100 as HighestInfection_Rate
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE continent is not null
+GROUP BY Location, population
+ORDER BY HighestInfection_Rate desc
+
+-- Showing countries with highest death count per population
+
+SELECT location, MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE continent is not null
+GROUP BY Location
+ORDER BY TotalDeathCount desc
+
+
+-- CONTINENT RESULTS
+
+-- Showing continents with highest death count per population
+
+SELECT continent, MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE continent is not null
+GROUP BY continent
+ORDER BY TotalDeathCount desc
+
+
+-- GLOBAL RESULTS
+
+SELECT date, SUM(new_cases) as total_cases, SUM(CAST(new_deaths as int)) as total_deaths, SUM(CAST(new_deaths as int))/SUM(new_cases)*100 as Death_Rate
+FROM `covid19-374023.Covid19.covid_deaths`
+WHERE continent is not null
+GROUP BY date
+ORDER BY 1,2
+
